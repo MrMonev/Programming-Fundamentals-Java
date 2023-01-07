0955e29574aa3b929767ec1f8e8fe775afaeac7e
MrMonev <121023245+MrMonev@users.noreply.github.com>
2023-01-07 10:55:39 +0000

    Update TreasureHunt.java

--- FundamentalsDay6/TreasureHunt.java
+++ FundamentalsDay6/TreasureHunt.java
@@ -3,13 +3,28 @@ import java.util.Scanner;
 public class TreasureHunt {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        // all items stored it inside in our chestBox[]
+
         String[] chest = scanner.nextLine().split("\\|");
 
+        // commands from console
         String input = scanner.nextLine();
 
         while (!input.equals("Yohoho!")) {
+
+            // if the command is !Yohoho - we need new String[]
+            // To keep all commands and separate with  " ";
+
             String[] everyPart = input.split(" ");
 
+            // Checking the command with index [0] - then we create for loop
+            // our index must start with index [1] - just because we separate our commands,
+            // and we already use the first index witch is [0]
+            // Then checking our chestBox with the items what we have inside.
+            // For this we need (boolean) is its true brake, if it's false
+            // create new string chest with all elements which they are not in our chestBox
+            // and add them with space (" ")
             switch (everyPart[0]) {
                 case "Loot":
                     for (int i = 1; i < everyPart.length; i++) {
@@ -26,6 +41,9 @@ public class TreasureHunt {
                         }
                     }
                     break;
+                // Remove the element at the given position (console) and add it to the end
+                // of the chestBox.If the index is invalid, skip the command.
+
                 case "Drop":
                     int number = Integer.parseInt(everyPart[1]);
 
@@ -40,6 +58,9 @@ public class TreasureHunt {
                     }
                     break;
                 case "Steal":
+                    //	Someone steals the last count element items.
+                   // If there are fewer items than the given count, remove as many as there are.
+
                     int stealItems = Integer.parseInt(everyPart[1]);
 
                     if (stealItems >= 0 && stealItems < chest.length) {
@@ -69,9 +90,12 @@ public class TreasureHunt {
             }
             input = scanner.nextLine();
         }
+        //In the end, output the average treasure gain, which is the sum of all chest items
+        // length divided by the count of all items inside the chest formatted to the second decimal point:
 
-        String chestCount = String.join("", chest);
+        String chestCount = String.join(" ", chest);
         int counter = 0;
+
         for (int i = 0; i < chestCount.length(); i++) {
             counter++;
         }
