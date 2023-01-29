79be861050e3654490372edaa26a7ae5ead8af42
MrMonev <121023245+MrMonev@users.noreply.github.com>
2023-01-29 15:32:05 +0000

    Objects and Classes - Lab

--- Objects and Classes - Lab/Students2.java
+++ Objects and Classes - Lab/Students2.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class Students2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
