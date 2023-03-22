bc906976d81e530021ee16827dcb87a0cd4d6cbf
MrMonev <121023245+MrMonev@users.noreply.github.com>
2023-03-22 18:28:42 +0000

    Regular Expressions - Lab

---
+++ Regular Expressions - Lab/MatchFullName.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatchFullName {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        String regex = "\\b[A-Z][a-z]+ [A-Z][a-z]+\\b";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        // true or false matcher.find()
+        while (matcher.find()) {
+            String fullName = matcher.group();
+            System.out.print(fullName + " ");
+        }
+    }
+}
---
+++ Regular Expressions - Lab/MatchPhoneNumber.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatchPhoneNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        String regex = "\\+359([ -])2\\1\\d{3}\\1\\d{4}\\b";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        List<String> numbersList = new ArrayList<>();
+
+        while (matcher.find()) {
+            String validPhoneNumber = matcher.group();
+
+            numbersList.add(validPhoneNumber);
+
+        }
+        System.out.println(String.join(", ",numbersList));
+    }
+}
